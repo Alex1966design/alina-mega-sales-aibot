dea95715fc55d40d5caa9d0837a1af3e50233f7f
Alex1966design <a.kamenetsky@mail.ru>
2025-07-09 14:49:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import os
-from telegram.ext import Application, CommandHandler
-from dotenv import load_dotenv
 
-load_dotenv()
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+python
+from telegram.ext import Application, CommandHandler
+
+TELEGRAM_TOKEN = "7598269211:AAH5zTrpyfQ5R1fGUS6M8rSi_vD-GgE_DOI"
 
 async def start(update, context):
     await update.message.reply_text("Привет! Я Алина — нейро-чемпион по продажам 💬")
 
 def main():
+    print(f"TELEGRAM_TOKEN: {repr(TELEGRAM_TOKEN)}")  # Выведет токен в лог
     app = Application.builder().token(TELEGRAM_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
